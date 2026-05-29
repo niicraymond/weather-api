49ccf7c81a323282b001b700f8c1423cb6a486f2
Nicole Raymond <nicoleraymond@capgemini.com>
2026-05-29 13:44:41 +0100

    geocoding API call for long and lat

--- src/main/java/com/weatherapi/weather_api/service/WeatherService.java
+++ src/main/java/com/weatherapi/weather_api/service/WeatherService.java
@@ -1,10 +1,17 @@
 package com.weatherapi.weather_api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import com.weatherapi.weather_api.model.LocationSearchResponse;
 
 @Service
 public class WeatherService {
     public String getWeather(String location) {
-        return "The weather is good in " + location;
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://geocoding-api.open-meteo.com/v1/search?name=" + location;
+        LocationSearchResponse response = restTemplate.getForObject(url, LocationSearchResponse.class);
+        double lat = response.getResults().get(0).getLatitude();
+        double lon = response.getResults().get(0).getLongitude();
+        return "Lat: " + lat + ", Lon: " + lon;
     }
 }
